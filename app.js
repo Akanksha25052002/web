9e12667001e8125cfeca23eadfb1a6d103f6c896
Akanksha25052002 <akankshashrivastava2022@vitbhopal.ac.in>
2023-02-28 22:41:02 +0530

    House form

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 (function(){
-let screen=document.querySelector('.screen');
-let buttons=document.querySelectorAll('.btn');
-let clear=document.querySelector('.btn-clear');
-let equal=document.querySelector('.btn-equal');
+// const ab=()=>{
+
+    let screen=document.querySelector('.screen');
+    let buttons=document.querySelectorAll('.btn');
+    let clear=document.querySelector('.btn-clear');
+    let equal=document.querySelector('.btn-equal');
+
+    // buttons.addEventListener('click',function(button)){
+
+    // }
+
+// }
 
 buttons.forEach(function(button){
     button.addEventListener('click', function(e){
@@ -11,15 +19,29 @@ buttons.forEach(function(button){
     })
 });
 
-equal.addEventListener('click',function (e) {
-        if (screen.value === '') {
-            screen.value = "";
-        } else {
-            let answer = eval(screen.value);
-            screen.value = answer;
+equal.addEventListener('click',function(e){
+    if(screen.value ==='') {
+         screen.value = "Please enter";
+    } else {
+         let answer= eval(screen.value);
+        screen.value = answer;
         }
     })
+
+// equal.addEventListener('click',function(e){
+//     if(screen.value==''){
+//         let answer=eval(screen.value);
+//         screen.value=answer;
+//     }
+//     else{
+//         let answer=eval(screen.value);
+//         answer
+//     }
+// })
+/
+
 clear.addEventListener('click',function(e){
     screen.value="";
 })
 })();
+
